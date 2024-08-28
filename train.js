f9031c79e802fb3529c36bdeb8cb4342e25324f3
David <odilovrakhmidin@gmail.com>
2024-08-28 20:01:13 +0900

    fix: train.js history

--- train.js
+++ train.js
@@ -7,8 +7,7 @@ function getReverse(string) {
 
 console.log(getReverse("hello"));
 
-/* 
-TASK D
+/* TASK D
 
 function checkContent(a, b) {
   let array1 = a.split("").sort().join("");
@@ -99,3 +98,117 @@ console.log(shop.qabul("non", 4));
 console.log(shop.sotish("non", 3));
 console.log(shop.sotish("cola", 2));
 console.log(shop.qoldiq()); */
+
+/* B - TASK;
+function countDigits(str) {
+  let count = 0;
+  for (let a = 0; a < str.length; a++) {
+    if (!isNaN(str[a])) {
+      count++;
+    }
+  }
+  return count;
+} 
+
+console.log(countDigits("ad2a54y79wet0sfgb9"));
+*/
+
+/* A-TASK
+
+function countlatter(e, word) {
+  let count = 0;
+
+  for (let a = 0; a < word.length; a++) {
+    if (word[a] === e) {
+      console.log("count:", count);
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countlatter("e", "engineer")); */
+
+/* console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba boling", //0-20
+  "togri boshliq tanlang va koproq hato qiling", //20-30
+  "ozingizga ishlashni boshlang", //30-40
+  "siz kuchli bolgan narsalarni qiling", //40-50
+  "yoshlarga investitsiya qiling", //50-60
+  "endi dam oling, foydasi yoq endi", //60
+]; */
+
+// => CALL BACK FUNCTIONS
+
+/* function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("insert a number", "null");
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setInterval(function () {
+      callback(null, list[5]);
+    }, 1000);
+  }
+}
+  */
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+// console.log("passed here 1");
+//  => ASYNC FUNCTION
+
+/* async function maslahatBering(a) {
+  if (typeof a !== "number") throw new Error("insert a number");
+  else if (a <= 20) return list[0];
+  else if (a > 20 && a <= 30) return list[1];
+  else if (a > 30 && a <= 40) return list[2];
+  else if (a > 40 && a <= 50) return list[3];
+  else if (a > 50 && a <= 60) return list[4];
+  else {
+    return new Promise((resolve, reject) => {
+      setInterval(() => {
+        resolve(list[5]);
+      }, 1000);
+    });
+  }
+} */
+
+// => call via then/catch
+
+/* console.log("passed here 0");
+maslahatBering(15)
+  .then((data) => {
+    console.log("javob:", data);
+  })
+  .catch((err) => {
+    console.log("ERROR:", err);
+  });
+console.log("passed here 1"); */
+
+// => call via async/await
+
+/* async function run() {
+  let javob = await maslahatBering(65);
+  console.log(javob);
+  javob = await maslahatBering(65);
+  console.log(javob);
+  javob = await maslahatBering(40);
+  console.log(javob);
+  javob = await maslahatBering(50);
+  console.log(javob);
+  javob = await maslahatBering(59);
+  console.log(javob);
+  javob = await maslahatBering(61);
+  console.log(javob);
+}
+
+run(); */
